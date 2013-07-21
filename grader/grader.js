47183d478581b9bf8da2f390d4a52f17e218efd7
Ubuntu <ubuntu@ip-172-31-41-14.us-west-2.compute.internal>
2013-07-21 23:28:01 +0000

    Styled index

--- grader/grader.js
+++ grader/grader.js
@@ -24,8 +24,20 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.github.com";
+
+ var assertURLExists = function(url) {
+    rest.get(url).on('complete',function(result) {
+    if (result instanceof Error) {
+      process.exit(1);
+     }
+   return result;
+   });
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +67,12 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var  checkWebAddress = function(weburl, checksfile){
+    rest.get(weburl).on('complete', function(result) {
+      
+   });   
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +83,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+        .option('-u, --url <url>', 'Path to URL', clone(assertURLExists), URL_DEFAULT)
+       .parse(process.argv);
+ //   if (program.url)
+        var checkJson = checkWebAddress(program.url, program.checks);
+  //  else
+        var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(outJson);y
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
